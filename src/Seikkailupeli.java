cc593757bde25b40f2329817eb24a89d4e482628
Jari Haavisto <jari.haavisto@gmail.com>
2017-10-17 10:03:33 +0300

    projektin aloitus

---
+++ src/Seikkailupeli.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 17/10/2017.
+ */
+public class Seikkailupeli {
+    public static void main(String[] args) {
+        System.out.println("Ei oo kummonen peli vielä");
+    }
+}
